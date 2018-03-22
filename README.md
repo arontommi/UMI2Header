fa26d6437227b2277e413d382cb2e7e19e125bed
arontommi <arontommi@gmail.com>
2018-03-22 16:13:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Simple script that takes an index read and adds it to the end of the header of reads.
+I used it after doing bcl2fastq for umi data.
+
+Things to note:
+make sure that nothing is removed based on quality in bcl2fastq.
+In this example i used it on samples that had a I1 read of 8 bp and I2 of 10 bp
+do it like this :
+
+
+bcl2fastq --runfolder-dir $1 -p 12 --output-dir $1/fastq_files \
+--use-bases-mask Y*,I8,Y10,Y*  --minimum-trimmed-read-length 0 \
+--mask-short-adapter-reads 0 --create-fastq-for-index-reads --no-lane-splitting
+
+this results in 4 output files: index one, read1 read2 and read3.
+Index one is the one used for demulitplexing, while read 2 is the umi data.
+
+i suggest renaming the data to read1 read2 and umi. (renaming read2 to umi and read3 to read2
+, confusing i know)
+
+i then use the script like this :
+
+python UMI2Header/U2H.py fix_barcode \
+ --f1 read1.fastq.gz \
+ --f2 read2.fastq.gz \
+ --barcode umi.fastq.gz
+
+this results in an header to change from
+@blaba:56:blabla:1:11101:10799:1082 3:N:0:AAGCCTAA
+
+to this >
+@blaba:56:blabla:1:11101:10799:1082 3:N:0:AAGCCTAA_TACCTCCTGT
+
+this can then be aligned. i use bowtie2 with the "--sam-no-qname-trunc" so that the UMI tag
+will make it to the bam file
+
+deduplication can then be done with UMI_tools dedup (https://github.com/CGATOxford/UMI-tools)
---
+++ U2H.py
@@ -0,0 +1,72 @@
+import os
+import gzip
+import fire
+from subprocess import call
+
+class Umi2Header(object):
+    """input fastqfiles"""
+
+    def fq(self, filename):
+        if filename.endswith('.gz'):
+            fastq = gzip.open(filename, 'rb')
+        else:
+            fastq = open(filename, 'rb')
+        with fastq as f:
+            while True:
+                l1 = f.readline()
+                if not l1:
+                    break
+                l2 = f.readline()
+                l3 = f.readline()
+                l4 = f.readline()
+                read = [l1, l2, l3, l4]
+                yield read
+
+    def gzip_suproccess(self, file):
+        "since python gzip takes for ever"
+        path = os.getcwd() + '/'
+        pathfile = path + file
+        call(['gzip', '{}'.format(pathfile)])
+
+    def remove_previous_umi(self,file):
+        path = os.getcwd() + '/'
+        if os.path.isfile(path + file.split('.')[0] + '.UMI.fastq.gz'):
+            os.remove(path + file.split('.')[0] + '.UMI.fastq.gz')
+        else:
+            pass
+
+    def fix_barcode(self, f1, f2, barcode):
+        self.remove_previous_umi(f1)
+        self.remove_previous_umi(f2)
+        filename1 = f1.split('.')[0] + '.UMI.fastq'
+        filename2 = f2.split('.')[0] + '.UMI.fastq'
+        file1 = open(filename1, "ab")
+        file2 = open(filename2, "ab")
+
+        for r1, r2, r3 in zip(self.fq(f1), self.fq(f2), self.fq(barcode)):
+            file1.writelines([r1[0].rstrip() + b"_" + r3[1],
+                              r1[1],
+                              r1[2],
+                              r1[3]])
+            file2.writelines([r2[0].rstrip() + b"_" + r3[1],
+                              r1[1],
+                              r1[2],
+                              r1[3]])
+        file1.close()
+        file2.close()
+        self.gzip_suproccess(filename1)
+        self.gzip_suproccess(filename2)
+
+
+
+def testmain():
+     u2h = Umi2Header
+     u2h.fix_barcode('~/TestingArea/')
+
+
+def main():
+   fire.Fire(Umi2Header)
+
+
+if __name__ == '__main__':
+    main()
